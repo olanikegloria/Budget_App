00c1e90b2afa5a74e55aaac89bfdfe2a2e7569ec
olanike olatunji <olanikegloria2020@gmail.com>
2023-10-20 22:19:11 +0100

    added required changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,8 +36,20 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  # SMTP
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:'smtp.gmail.com',
+    port: 587,
+    domain: 'wallet-aid.onrender.com',
+    authentication: :plain,
+    enable_starttls_auto: true,
+    user_name: 'cohenhoward340@gmail.com',
+    password: 'fmftvxmndmrnjjow',
+  }
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -59,7 +71,6 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
